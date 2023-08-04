5cb679c90edbe464c5a916e1703c5d99757dc410
Letícia Escobar <leticiaescobaraujo@gmail.com>
2023-08-03 22:03:10 -0300

    aula 1 - array e map

--- src/App.js
+++ src/App.js
@@ -8,64 +8,44 @@ import { Component } from 'react'
 // function App() {}
 
 class App extends Component {
-    constructor(props) {
-        // super chama o construtor da classe pai
-        super(props)
-        // this.handleClick = this.handleClick.bind(this)
-
-        this.state = {
-            name: 'Letícia',
-            counter: 0
-        }
-    }
-
-    // substituir o bind(this) pelo arrow function
-    handleClick = () => {
-        const { name } = this.state
-        console.log(`Cliquei em ${name}`)
-        this.setState({ name: 'Nome mudou' })
-    }
-
-    handleClickButton = () => {
-        const { counter } = this.state
-        console.log(`Cliquei no botão`)
-        this.setState({ counter: counter + 1 })
+    // criando um estado de posts
+    state = {
+        // criando um array de objetos
+        posts: [
+            {
+                id: 1,
+                title: 'Título 1',
+                body: 'Corpo 1'
+            },
+            {
+                id: 2,
+                title: 'Título 2',
+                body: 'Corpo 2'
+            },
+            {
+                id: 3,
+                title: 'Título 3',
+                body: 'Corpo 3'
+            }
+        ]
     }
 
     render() {
         // Todos os estados precisam de uma função para alterá-los
-        const { name, counter } = this.state
+        const { posts } = this.state
 
         return (
-            // className é obrigatório, pois class é uma palavra reservada do JS
-            // react não permite fazer return de mais de uma tag, por isso, é necessário colocar tudo dentro de uma div ou de um react fragment (<> </>)
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <p>
-                        <span onClick={this.handleClick}>
-                            {name} - {counter}
-                        </span>
-                        <br />
-                        <br />
-                        <button onClick={this.handleClickButton}>Botão</button>
-                    </p>
-                    <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                    >
-                        {/* Dentro das chaves eu posso colocar códigos js */}A
-                        soma de 1 + 1 é {1 + 1}
-                    </a>
-                </header>
+                {/* se eu quero um retorno preciso colocar parênteses, se eu não quiser retorno, posso usar chaves */}
+                {posts.map(post => (
+                    <div key={post.id}>
+                        <h1>{post.title}</h1>
+                        <h3>{post.body}</h3>
+                    </div>
+                ))}
             </div>
         )
     }
 }
 
-// O export default é obrigatório, pois é o que permite que o componente seja importado em outros arquivos
 export default App
-
-// extensão jsx é opcional, mas é uma boa prática para diferenciar os arquivos que contém html junto com js
