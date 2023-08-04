87c3ead4c245241a732f74d7d49e3ac3a83877ef
Letícia Escobar <leticiaescobaraujo@gmail.com>
2023-08-03 21:34:47 -0300

    primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { render } from '@testing-library/react'
+import { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Componentes baseados em estados são chamados de componentes stateful
+// Componentes baseados em funções são chamados de componentes stateless
+// function App() {}
+
+class App extends Component {
+    constructor(props) {
+        // super chama o construtor da classe pai
+        super(props)
+        // this.handleClick = this.handleClick.bind(this)
+
+        this.state = {
+            name: 'Letícia',
+            counter: 0
+        }
+    }
+
+    // substituir o bind(this) pelo arrow function
+    handleClick = () => {
+        const { name } = this.state
+        console.log(`Cliquei em ${name}`)
+        this.setState({ name: 'Nome mudou' })
+    }
+
+    render() {
+        // Todos os estados precisam de uma função para alterá-los
+        const { name, counter } = this.state
+
+        return (
+            // className é obrigatório, pois class é uma palavra reservada do JS
+            // react não permite fazer return de mais de uma tag, por isso, é necessário colocar tudo dentro de uma div ou de um react fragment (<> </>)
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        <span onClick={this.handleClick}>
+                            {name} - {counter}
+                        </span>
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        {/* Dentro das chaves eu posso colocar códigos js */}A
+                        soma de 1 + 1 é {1 + 1}
+                    </a>
+                </header>
+            </div>
+        )
+    }
 }
 
-export default App;
+// O export default é obrigatório, pois é o que permite que o componente seja importado em outros arquivos
+export default App
+
+// extensão jsx é opcional, mas é uma boa prática para diferenciar os arquivos que contém html junto com js
